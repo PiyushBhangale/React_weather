f524c081f8844ad32a2d727d178e53cb389bcaed
PiyushBhangale <piyushbhangale1995@gmail.com>
2018-11-29 10:36:34 +0530

    css changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,8 @@ import React,{Component} from 'react'
 const Form=(props)=>{
         return(
             <form onSubmit={props.getWeather}>
-                <input type="text" name="city" placeholder="city.."></input>
-                <input type="text" name="country" placeholder="country.."></input>
+                <input type="text" name="city" placeholder="City"></input>
+                <input type="text" name="country" placeholder="Country"></input>
                 <button>Get Weather</button>
             </form>
         )
