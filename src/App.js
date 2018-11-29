025c3caa0e9ec266a4bd194cb734bd48c98ae3f2
PiyushBhangale <piyushbhangale1995@gmail.com>
2018-11-29 11:36:22 +0530

    https change

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component{
     e.preventDefault()
     const city=e.target.elements.city.value
     const country=e.target.elements.country.value
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data=await api_call.json()
     if(city && country )
     {
